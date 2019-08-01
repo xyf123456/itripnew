330790dac73a53bde380496649d6b4da241cdbab
xuyunfei <905231404@qq.com>
2019-08-01 17:26:49 +0800

    整合了auth模块，但是未能实现redis的相关配置

---
+++ itripauth/src/main/java/cn/itrip/auth/Itripauth.java
@@ -0,0 +1,14 @@
+package cn.itrip.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class Itripauth {
+    public static void main(String[] args) {
+        SpringApplication.run(Itripauth.class, args);
+    }
+}
