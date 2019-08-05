09938b31e7c8d5c0afe1c3429f3e6671cbffc905
xuyunfei <905231404@qq.com>
2019-08-05 17:39:13 +0800

    整合solr模块完成1

---
+++ itripsearch/src/main/java/cn/itrip/Itripsearch.java
@@ -0,0 +1,18 @@
+package cn.itrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Itripsearch
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+        SpringApplication.run(Itripsearch.class,args);
+    }
+}
---
+++ itripsearch/src/test/java/cn/itrip/ItripsearchTest.java
@@ -0,0 +1,20 @@
+package cn.itrip;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Itripsearch.
+ */
+public class ItripsearchTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
